dd2647c765f1e0c0d1b19ed2d76e813fcc4b79b4
amahmoodi1379-glitch <a.mahmoodi1379@gmail.com>
2025-11-29 04:24:22 +0330

    Update client.ts

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,10 +1,16 @@
 import { Env } from "../types";
 
+// روشن کردن بررسی ارتباط جداول برای هر درخواست
+async function enableForeignKeys(env: Env) {
+  await env.DB.prepare("PRAGMA foreign_keys = ON;").run();
+}
+
 export async function queryOne<T>(
   env: Env,
   sql: string,
   params: any[] = []
 ): Promise<T | null> {
+  // await enableForeignKeys(env); // اگر سرعت خیلی مهم نیست این خط را فعال کن
   const stmt = env.DB.prepare(sql);
   const res = await stmt.bind(...params).first();
   if (!res) return null;
@@ -27,11 +33,12 @@ export async function execute(
   sql: string,
   params: any[] = []
 ): Promise<any> {
+  // برای دستورات حذف و آپدیت حتما باید فعال باشد
+  await enableForeignKeys(env);
   const stmt = env.DB.prepare(sql);
   return await stmt.bind(...params).run();
 }
 
-// NEW: آماده‌سازی دستور برای Batch
 export function prepare(
   env: Env,
   sql: string,
